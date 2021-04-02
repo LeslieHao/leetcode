4fec04326d49e6aac5817c68064cb3c0107d1c8b
haohao <haohao@58.com>
2021-04-02 14:24:37 +0800

    setZeroes

---
+++ src/main/java/com/hh/leetcode/midium/arr/SetZeroes.java
@@ -0,0 +1,71 @@
+package com.hh.leetcode.midium.arr;
+
+/**
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ *
+ * 进阶：
+ *
+ * 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+ * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+ * 你能想出一个仅使用常量空间的解决方案吗？
+ *
+ *
+ * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+ * 输出：[[1,0,1],[0,0,0],[1,0,1]]
+ *
+ *
+ * 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ * 输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ *
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-medium/xvmy42/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ *
+ * @author HaoHao
+ * @date 2021/4/1 3:45 下午
+ */
+public class SetZeroes {
+
+    /**
+     * 我们可以用两个标记数组分别记录每一行和每一列是否有零出现。
+     *
+     * 具体地，我们首先遍历该数组一次，如果某个元素为 00，那么就将该元素所在的行和列所对应标记数组的位置置为 true。
+     * 最后我们再次遍历该数组，用标记数组更新原数组即可。
+     *
+     * 时间复杂度：O(mn)O(mn)，其中 mm 是矩阵的行数，nn 是矩阵的列数。我们至多只需要遍历该矩阵两次。
+     *
+     * 空间复杂度：O(m+n)O(m+n)，其中 mm 是矩阵的行数，nn 是矩阵的列数。我们需要分别记录每一行或每一列是否有零出现。
+     *
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] mZero = new boolean[m];
+        boolean[] nZero = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    mZero[i] = true;
+                    nZero[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mZero[i] || nZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
