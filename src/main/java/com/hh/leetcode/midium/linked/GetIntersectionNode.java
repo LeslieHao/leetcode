f32f8b7a465d3327c8dac79f38debea852edc156
haohao <haohao@58.com>
2021-11-22 22:12:30 +0800

    链表相交两种

--- src/main/java/com/hh/leetcode/midium/linked/GetIntersectionNode.java
+++ src/main/java/com/hh/leetcode/midium/linked/GetIntersectionNode.java
@@ -8,13 +8,50 @@ import com.hh.leetcode.primary.linked.ListNode;
  */
 public class GetIntersectionNode {
 
+    /**
+     * 走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路。
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public static ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while (pa != pb) {
+            if (pa == null) {
+                pa = headB;
+            }else {
+                pa = pa.next;
+            }
+
+            if (pb == null) {
+                pb = headA;
+            } else {
+                pb = pb.next;
+            }
+        }
+        return pa;
+    }
+
+
+
+        /**
+         * 这种解法只适合仅当相交时值相等的情况
+         * @param headA
+         * @param headB
+         * @return
+         */
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // 求出长度差h
         int A = 0;
         int B = 0;
         ListNode tempA = headA;
         ListNode tempB = headB;
-        // 高度差
+        // 长度差
         int h = 0;
         while (headA != null) {
             A++;
@@ -52,24 +89,22 @@ public class GetIntersectionNode {
     }
 
     public static void main(String[] args) {
-        ListNode a = new ListNode(4);
-        ListNode b = new ListNode(1);
-        ListNode c = new ListNode(8);
-        ListNode d = new ListNode(4);
-        ListNode e = new ListNode(5);
+        int[] arr1 = {4, 1, 8, 4, 5};
+        int[] arr2 = {5, 6, 1, 8, 4, 5};
 
+        ListNode node = getNode(arr1);
+        ListNode node1 = getNode(arr2);
+        System.out.println(getIntersectionNode1(node, node1).val);
+    }
 
-        ListNode f = new ListNode(5);
-        ListNode g = new ListNode(0);
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-
-        f.next = g;
-        g.next = b;
-
-        getIntersectionNode(a, f);
+    private static ListNode getNode(int[] arr) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode tail = head;
+        for (int i = 1; i < arr.length; i++) {
+            tail.next = new ListNode(arr[i]);
+            tail = tail.next;
+        }
+        return head;
     }
 
 }
