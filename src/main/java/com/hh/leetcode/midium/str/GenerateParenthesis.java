e412ad9c4e61762d2d6c8ed86d3f3943c6ff898e
haohao <haohao@58.com>
2021-12-16 23:01:53 +0800

    优先队列/滑动窗口

---
+++ src/main/java/com/hh/leetcode/midium/str/GenerateParenthesis.java
@@ -0,0 +1,30 @@
+package com.hh.leetcode.midium.str;
+
+/**
+ * 数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：["()"]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 8
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ *
+ * @author HaoHao
+ * @date 2021/12/9 9:53 下午
+ */
+public class GenerateParenthesis {
+
+}
